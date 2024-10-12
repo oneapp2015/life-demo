999b611687d793f5007c7e1cfb90566411d2d50a
oneapp2015 <15252340@qq.com>
2024-10-12 21:29:41 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+    ifstream file("input.txt");
+    ofstream out("out.txt");
+    int n, x, y, z, g;
+    char c;
+    const int N=41;
+    bool s0[N][N]={}, s1[N][N]={};
+    file >> n;
+    for(int i=0;i<n;i++)
+    {
+        file >> x >> y;
+        s0[x][y]=true;
+    }
+    file.close();
+    while(1)
+    {
+//        c='a';
+//        while(c!='x')
+//        {
+//            cin >> c;
+//            if(c=='o')
+//            {
+//                g=0;
+//                for(int i=1;i<N-1;i++)
+//                {
+//                    for(int j=1;j<N-1;j++)
+//                    {
+//                        if(s0[i][j]==true)
+//                        {
+//                            out << i << " " << j << endl;
+//                            g++;
+//                        }
+//                    }
+//                }
+//                out << g << endl;
+//            }
+//            system("cls");
+//        }
+        system("cls");
+        for(int i=1;i<N-1;i++)
+        {
+            for(int j=1;j<N-1;j++)
+            {
+                z=0;
+                if(s0[i-1][j-1]==true)
+                {
+                    z++;
+                }
+                if(s0[i-1][j]==true)
+                {
+                    z++;
+                }
+                if(s0[i][j-1]==true)
+                {
+                    z++;
+                }
+                if(s0[i+1][j+1]==true)
+                {
+                    z++;
+                }
+                if(s0[i][j+1]==true)
+                {
+                    z++;
+                }
+                if(s0[i+1][j]==true)
+                {
+                    z++;
+                }
+                if(s0[i+1][j-1]==true)
+                {
+                    z++;
+                }
+                if(s0[i-1][j+1]==true)
+                {
+                    z++;
+                }
+                if(s0[i][j]==true)
+                {
+                    if(z==2||z==3)
+                    {
+                        s1[i][j]=true;
+                    }
+                    else
+                    {
+                        s1[i][j]=false;
+                    }
+                }
+                else
+                {
+                    if(z==3)
+                    {
+                        s1[i][j]=true;
+                    }
+                    else
+                    {
+                        s1[i][j]=false;
+                    }
+                }
+            }
+        }
+        for(int i=1;i<N-1;i++)
+        {
+            for(int j=1;j<N-1;j++)
+            {
+                if(s1[i][j]==true)
+                {
+                    cout << " #";
+                }
+                else
+                {
+                    cout << " .";
+                }
+            }
+            cout << endl;
+        }
+        swap(s0, s1);
+        Sleep(350);
+    }
+}
